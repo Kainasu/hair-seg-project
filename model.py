6d07fe6f221375c74927c9adb5e021e0e275e31e
Nicolas Do <61271171+Kainasu@users.noreply.github.com>
2023-01-05 21:18:29 +0100

    Refactoring

--- model.py
+++ model.py
@@ -3,61 +3,43 @@ from keras.models import Model
 from keras.losses import BinaryCrossentropy
 from keras.metrics import BinaryIoU
 
+def conv_block(inputs, filters):
+    x = Conv2D(filters, (3,3) , activation='relu', padding="same")(inputs)
+    x = BatchNormalization()(x)
+    x = Conv2D(filters, (3,3), activation='relu', padding="same")(x)
+    x = BatchNormalization()(x)
+    return x
+
+def deconv_block(inputs, filters, skip_connection):
+    x = concatenate([Conv2DTranspose(filters, (2, 2), strides=(2, 2), padding='same')(inputs), skip_connection], axis=3)
+    x = conv_block(x, filters)
+    return x
+
 def create_unet(image_size=(128,128,3)):
     """returns unet model"""
 
     inputs = Input(image_size)
 
     #Downsampling
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
-    conv1 = BatchNormalization()(conv1)
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
-    conv1 = BatchNormalization()(conv1)
+    conv1 = conv_block(inputs, 32)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-    conv2 = BatchNormalization()(conv2)
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-    conv2 = BatchNormalization()(conv2)
+    conv2 = conv_block(pool1, 64)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
-    conv3 = BatchNormalization()(conv3)
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
-    conv3 = BatchNormalization()(conv3)
+    conv3 = conv_block(pool2, 128)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
-    conv4 = BatchNormalization()(conv4)
-    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
-    conv4 = BatchNormalization()(conv4)
+    conv4 = conv_block(pool3, 256)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
-    conv5 = BatchNormalization()(conv5)
-    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
-    conv5 = BatchNormalization()(conv5)
+    conv5 = conv_block(pool4, 512)
 
     #Upsampling
-    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3) #Was conv5 instead of conv6a
-    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
-    conv6 = BatchNormalization()(conv6)
-    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
-    conv6 = BatchNormalization()(conv6)
-    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
-    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
-    conv7 = BatchNormalization()(conv7)
-    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
-    conv7 = BatchNormalization()(conv7)
-    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
-    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
-    conv8 = BatchNormalization()(conv8)
-    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
-    conv8 = BatchNormalization()(conv8)
-    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
-    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
-    conv9 = BatchNormalization()(conv9)
-    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
-    conv9 = BatchNormalization()(conv9)
-    conv10 = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(conv9)
+    dconv1 = deconv_block(conv5, 256, conv4)
+    dconv2 = deconv_block(dconv1, 128, conv3)
+    dconv3 = deconv_block(dconv2, 64, conv2)
+    dconv4 = deconv_block(dconv3, 32, conv1)
 
-    model = Model(inputs=[inputs], outputs=[conv10])
+    outputs = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(dconv4)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
 
     model.compile(optimizer='adam', loss=BinaryCrossentropy(), metrics=['acc', BinaryIoU()])
 
