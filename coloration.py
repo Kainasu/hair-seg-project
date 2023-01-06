8463518bf609e83aa751a37a80347643dbe0e5fc
Kainasu <Kainasu@github.com>
2023-01-06 17:05:36 +0100

    Fix coloration

--- coloration.py
+++ coloration.py
@@ -4,14 +4,14 @@ import cv2
 def change_color(image, mask, color):
     color = color.lstrip('#')
     color = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
-    color = np.array(color)    
+    color = np.array(color)
     thresh = 0.7
     """Create 3 copies of the mask, one for each color channel"""
-    blue_mask = mask.copy()
+    blue_mask = mask.copy()    
     blue_mask[mask > thresh] = color[0]
     blue_mask[mask <= thresh] = 0
-
-    green_mask = mask.copy()
+    
+    green_mask = mask.copy()    
     green_mask[mask > thresh] = color[1]
     green_mask[mask <= thresh] = 0
 
@@ -28,9 +28,9 @@ def change_color(image, mask, color):
     mask_n[:, :, 0] = blue_mask
     mask_n[:, :, 1] = green_mask
     mask_n[:, :, 2] = red_mask
-
     alpha = 0.85
     beta = (1.0 - alpha)
-    out = cv2.addWeighted(image, alpha, mask_n, beta, 0.0)
+    out = cv2.addWeighted(image, alpha, mask_n, beta, 0.0)    
 
-    return (out*255).astype(np.uint8)
+    return out
+
--- segmentation.py
+++ segmentation.py
@@ -1,50 +1,66 @@
 import argparse
 import numpy as np
-from PIL import Image
+
 import matplotlib.pyplot as plt
 from keras.models import load_model
 import time
 from coloration import change_color
+import cv2
+
+def predict(image, model, height=128, width=128):
+    im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    model = load_model(model)
+    """Preprocess the input image before prediction"""
+    im = im / 255
+    im = cv2.resize(im, (height, width))
+    im = im.reshape((1,) + im.shape)
+    pred = model.predict(im)   
+    mask = pred.reshape((height, width))
+    return mask
+
 
 def predict_and_plot(img_path, model, color, mask_path=None):
     ncols = 2
-    
-    model = load_model(model)
-    img = np.asarray(Image.open(img_path).resize((128,128)).convert('RGB'))/255.
+
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (128, 128))
 
     if color is not None:
         ncols += 1
         col_color = 3        
 
     if mask_path is not None:
-        mask = np.asarray(Image.open(mask_path).resize((128,128)).convert('L'))/255.
+
+        mask = cv2.imread(mask_path)
+        mask = cv2.resize(mask, (128, 128))
         ncols += 1
         col_mask = 3 if ncols < 4 else 4
 
-    pred = model.predict(img[np.newaxis,:,:,:])
+    pred = predict(img, model)
     
     treshold = 0.7
     pred_mask = ((pred > treshold) * 255.)
     
-    fig, axes = plt.subplots(nrows=1, ncols=ncols)
-    plt.subplot(1,ncols,1)
+    fig, axes = plt.subplots(nrows=1, ncols=ncols)    
+    plt.subplot(1,ncols,1)    
     plt.imshow(img)
     plt.title("Original")
     plt.subplot(1,ncols,2)
-    plt.imshow(pred_mask[0])
+    plt.imshow(pred_mask)
     plt.title("pred")
 
     if color is not None:                
         plt.subplot(1,ncols,col_color)
-        plt.imshow(change_color(img, pred[0], color))
+        colored_img = change_color(img, pred, color) 
+        plt.imshow(cv2.cvtColor(colored_img, cv2.COLOR_BGR2RGB))
         plt.title("Colored photo")
 
     if mask_path is not None:
         plt.subplot(1,ncols, col_mask)
         plt.imshow(mask)
         plt.title("GT")
-        score = model.evaluate(img[np.newaxis,:,:,:], mask[np.newaxis,:,:])
-        print("accuacy : ", score[1])
+        # score = model.evaluate(img, mask[np.newaxis,:,:])
+        # print("accuacy : ", score[1])
     
     plt.show()
     
