6b8d29a323ba4d8a3460d773bf3e63e332c92d05
Nicolas Do <61271171+Kainasu@users.noreply.github.com>
2023-01-06 02:11:59 +0100

    mobile unet refactoring

--- mobile_model.py
+++ mobile_model.py
@@ -2,6 +2,20 @@ from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv2
 from keras.models import Model
 from keras.losses import BinaryCrossentropy
 from keras.metrics import BinaryIoU
+from keras.applications import MobileNetV2
+
+def conv_block(inputs, filters):
+    x = Conv2D(filters, (3,3) , activation='relu', padding="same")(inputs)
+    x = BatchNormalization()(x)
+    x = Conv2D(filters, (3,3), activation='relu', padding="same")(x)
+    x = BatchNormalization()(x)
+    return x
+
+def deconv_block(inputs, filters, skip_connection):
+    x = concatenate([Conv2DTranspose(filters, (2, 2), strides=(2, 2), padding='same')(inputs), skip_connection], axis=3)
+    x = conv_block(x, filters)
+    return x
+
 
 def bottleneck_block(inputs, filters, strides=(1,1), expansion_factor=6, resi=False):
     expand = inputs.shape[-1] * expansion_factor    
@@ -17,101 +31,57 @@ def bottleneck_block(inputs, filters, strides=(1,1), expansion_factor=6, resi=Fa
         return Add()([m, inputs])
     return m
 
-
-
 def inverted_residual_block(inputs, filters, strides=(1,1), expansion_factor=6, n=1):
     m = bottleneck_block(inputs, filters, strides, expansion_factor, False)
     for i in range(1, n):
         m = bottleneck_block(m, filters, (1,1), expansion_factor, True)
     return m
-    
-# Architecture inspired from https://pdfs.semanticscholar.org/44e1/3ee0ad72e84c13635a9716bb7a27f39d27a2.pdf
-def create_mobile_unet(image_size=(128,128,3)):
+
+def create_mobile_unet(image_size=(128,128,3), pretrained=True):
     """returns unet model"""
 
     inputs = Input(image_size)
     
     #Encoder
-    
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
-    conv1 = BatchNormalization()(conv1)
+    if pretrained:
+        encoder = MobileNetV2(include_top=False, input_tensor=inputs, alpha=0.5)
+        s1 = inputs               
+        s2 = encoder.get_layer("block_1_expand_relu").output    
+        s3 = encoder.get_layer("block_3_expand_relu").output    
+        s4 = encoder.get_layer("block_6_expand_relu").output    
 
-    bottleneck1 = inverted_residual_block(conv1, 16, strides=(1,1), expansion_factor=1)
+        out = encoder.get_layer("block_13_expand_relu").output   
 
-    bottleneck2 = inverted_residual_block(bottleneck1, 24, strides=(2,2), expansion_factor=6, n=2)
+    else : 
+        s1 = inputs
+        conv1 = Conv2D(32, (3, 3), activation='relu6', padding='same', strides=(2,2))(s1)
+        conv1 = BatchNormalization()(conv1)
 
-    bottleneck3 = inverted_residual_block(bottleneck2, 32, strides=(2,2), expansion_factor=6, n=3)
+        s2 = inverted_residual_block(conv1, 16, strides=(1,1), expansion_factor=1)
 
-    bottleneck4 = inverted_residual_block(bottleneck3, 64, strides=(2,2), expansion_factor=6, n=4)
+        s3 = inverted_residual_block(s2, 24, strides=(2,2), expansion_factor=6, n=2)
 
-    bottleneck5 = inverted_residual_block(bottleneck4, 96, strides=(1,1), expansion_factor=6, n =3)
+        s4 = inverted_residual_block(s3, 32, strides=(2,2), expansion_factor=6, n=3)
 
-    bottleneck6 = inverted_residual_block(bottleneck5, 160, strides=(2,2), expansion_factor=6, n=3)
+        bottleneck = inverted_residual_block(s4, 64, strides=(2,2), expansion_factor=6, n=4)
 
-    bottleneck7 = inverted_residual_block(bottleneck6, 320, strides=(1,1), expansion_factor=6)
+        out = inverted_residual_block(bottleneck, 96, strides=(1,1), expansion_factor=6, n =3)
 
-    conv2 = Conv2D(1280, (1,1), activation='relu', padding='same')(bottleneck7)
-    conv2 = BatchNormalization()(conv2)
-    
-    #Decoder
+    #Upsampling
+    dconv1 = deconv_block(out, 256, s4)
+    dconv2 = deconv_block(dconv1, 128 , s3)
+    dconv3 = deconv_block(dconv2, 64, s2)
+    dconv4 = deconv_block(dconv3, 32, s1)
 
-    up6 = concatenate([Conv2DTranspose(320, (2, 2), strides=(1, 1), padding='same')(conv2), bottleneck7], axis=3) #Was conv5 instead of conv6a
-    conv6 = Conv2D(320, (3, 3), activation='relu', padding='same')(up6)
-    conv6 = BatchNormalization()(conv6)
-    conv6 = Conv2D(320, (3, 3), activation='relu', padding='same')(conv6)
-    conv6 = BatchNormalization()(conv6)
-    up7 = concatenate([Conv2DTranspose(160, (2, 2), strides=(1, 1), padding='same')(conv6), bottleneck6], axis=3)
-    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
-    conv7 = BatchNormalization()(conv7)
-    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
-    conv7 = BatchNormalization()(conv7)
-    up8 = concatenate([Conv2DTranspose(96, (2, 2), strides=(2, 2), padding='same')(conv7), bottleneck5], axis=3)
-    conv8 = Conv2D(96, (3, 3), activation='relu', padding='same')(up8)
-    conv8 = BatchNormalization()(conv8)
-    conv8 = Conv2D(96, (3, 3), activation='relu', padding='same')(conv8)
-    conv8 = BatchNormalization()(conv8)
+    outputs = Conv2D(1, (1, 1), activation='sigmoid', padding='same')(dconv4)
 
-    up9 = concatenate([Conv2DTranspose(64, (2, 2), strides=(1, 1), padding='same')(conv8), bottleneck4], axis=3)
-    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(up9)
-    conv9 = BatchNormalization()(conv9)
-    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv9)
-    conv9 = BatchNormalization()(conv9)
-    up10 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv9), bottleneck3], axis=3)
-    conv10 = Conv2D(32, (3, 3), activation='relu', padding='same')(up10)
-    conv10 = BatchNormalization()(conv10)
-    conv10 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv10)
-    conv10 = BatchNormalization()(conv10)
-    up11 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv10), bottleneck2], axis=3)
-    conv11 = Conv2D(32, (3, 3), activation='relu', padding='same')(up11)
-    conv11 = BatchNormalization()(conv11)
-    conv11 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv11)
-    conv11 = BatchNormalization()(conv11)
-    conv12 = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(conv11)
-
-    model = Model(inputs=[inputs], outputs=[conv12])
-
-
-
-    # d_bottleneck1 = inverted_residual_block(conv2, 96, strides=(2,2), expansion_factor=6)
-
-    # d_bottleneck2 = inverted_residual_block(d_bottleneck1, 32, strides=(2,2), expansion_factor=6)
-
-    # d_bottleneck3 = inverted_residual_block(d_bottleneck2, 24, strides=(2,2), expansion_factor=6)
-
-    # d_bottleneck4 = inverted_residual_block(d_bottleneck3, 16, strides=(2,2), expansion_factor=6)
-
-    # d_conv1 = Conv2DTranspose(16, (4, 4), strides=(2, 2), padding='same')(d_bottleneck4)
-    # conv3 = Conv2D(128, (1, 1), activation='relu', padding='same')(d_conv1)
-    # conv3 = BatchNormalization()(conv3)
-    # conv3 = Conv2D(128, (1, 1), activation='relu', padding='same')(conv3)
-    # conv3 = BatchNormalization()(conv3)
-
-    model = Model(inputs=[inputs], outputs=[conv2])
+    model = Model(inputs=[inputs], outputs=[outputs])
 
     model.compile(optimizer='adam', loss=BinaryCrossentropy(), metrics=['acc', BinaryIoU()])
 
     return model
     
+
 if __name__ == '__main__':
-    model = create_mobile_unet()
+    model = create_mobile_unet(pretrained=False)
     model.summary()
