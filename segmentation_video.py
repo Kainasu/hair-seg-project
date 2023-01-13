f710a0e61de8ea7dcf1f37c2abdaafa32fa4519a
bagtaib <bagtaib@ubuntu>
2023-01-13 15:00:24 +0100

    working versions of video and real time

--- segmentation_video.py
+++ segmentation_video.py
@@ -1,29 +1,36 @@
+import argparse
 import cv2
 import numpy as np
 from keras.models import load_model
 import matplotlib.pyplot as plt
+from coloration import change_color
 
-
-def apply_model_on_video(model, video_path, output_path):
+def apply_model_on_video(model, video_path, output_path, color):
     cap = cv2.VideoCapture(video_path)
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 10, (frame_width,frame_height))
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 15, (frame_width,frame_height))
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
-            frame = cv2.resize(frame, (128, 128))
-            frame = np.expand_dims(frame, axis=0)
-            frame = frame / 255.
-            frame = model.predict(frame)
+            frame_1 = cv2.resize(frame, (128, 128))
+            frame_1 = cv2.cvtColor(frame_1, cv2.COLOR_BGR2RGB)
+            
+            mask = np.expand_dims(frame_1, axis=0)
+            mask = mask / 255.
+            mask = model.predict(mask)
+            
             treshold = 0.7
-            pred_mask = ((frame > treshold) * 255.)
-            frame = pred_mask[0] # 128x128x1
-            frame = frame.astype(np.uint8)
-            frame = np.squeeze(frame, axis=2) # 128x128
-            frame= cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR) # 128x128x3
-            frame = cv2.resize(frame, (frame_width, frame_height)) # 1280x720x3
-            out.write(frame)
+            pred_mask = ((mask > treshold) * 255.)
+            mask = pred_mask[0]
+            mask = mask.astype(np.uint8)
+            mask = np.squeeze(mask, axis=2)
+            
+            result=change_color(frame_1, mask, color)
+            
+            result= cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+            result = cv2.resize(result, (frame_width, frame_height))
+            out.write(result)
             
         else:
             break
@@ -32,6 +39,11 @@ def apply_model_on_video(model, video_path, output_path):
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    model = load_model('model.h5')
-    apply_model_on_video(model, 'video_2.webm', 'output.mp4')
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', dest='model', action='store', required=True)    
+    parser.add_argument('--color', dest='color', action='store', required=True)
+    parser.add_argument('--input', dest='input', action='store', required=True)
+    parser.add_argument('--output', dest='output', action='store', required=True)
+    args = parser.parse_args()
+    model = load_model(args.model)
+    apply_model_on_video(model, args.input, args.output, args.color)
--- webcam.py
+++ webcam.py
@@ -3,59 +3,33 @@ import cv2
 from keras.models import load_model
 import numpy as np
 from coloration import change_color
-
-# superpose the mask on the frame and return the result 
-def process_frame(frame, model):
-
+    
+def process_frame_color(frame, model, color):
+    
     frame_1 = cv2.resize(frame, (128, 128))
+    frame_1 = cv2.cvtColor(frame_1, cv2.COLOR_BGR2RGB)
+    
     mask = np.expand_dims(frame_1, axis=0)
     mask = mask / 255.
     mask = model.predict(mask)
-    treshold = 0.7
-    pred_mask = ((mask > treshold) * 255.)
-    mask = pred_mask[0] # 128x128x1
-    mask = mask.astype(np.uint8)
-    mask = np.squeeze(mask, axis=2) # 128x128
     
-    # create image in which the pixels that are zero in the mask are retained from the original image, and the pixels that are nonzero in the mask are set to zero.
-    # invert the mask 
-    mask = cv2.bitwise_not(mask)
-    # Split the image into its three color channels
-    b,g,r = cv2.split(frame_1)
-
-    # Create a mask for each color channel
-    mask_b = cv2.bitwise_and(b, b, mask=mask)
-    mask_g = cv2.bitwise_and(g, g, mask=mask)
-    mask_r = cv2.bitwise_and(r, r, mask=mask)
-
-    # Combine the masks
-    mask = cv2.bitwise_or(mask_b, mask_g)
-    mask = cv2.bitwise_or(mask, mask_r)
-
-    # Overlay the mask on the original image to highlight the hair
-    result = cv2.bitwise_and(frame_1, frame_1, mask=mask)
-    result = cv2.resize(result, (frame.shape[1], frame.shape[0]))
-    return result
-    
-def process_frame_color(frame, model):
-    # ne marche pas à revoir
-    frame_1 = cv2.resize(frame, (128, 128))
-    mask = np.expand_dims(frame_1, axis=0)
-    mask = mask / 255.
-    mask = model.predict(mask)
     treshold = 0.7
     pred_mask = ((mask > treshold) * 255.)
     mask = pred_mask[0]
     mask = mask.astype(np.uint8)
     mask = np.squeeze(mask, axis=2)
-    result=change_color(frame_1, mask, '#ff0000')
-    #resize the result to the original frame size
-    result = cv2.resize(result, (frame.shape[1], frame.shape[0]))
+    
+    result=change_color(frame_1, mask, color)
+    
+    
+    result= cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+    result = cv2.resize(result, (frame.shape[1]*2, frame.shape[0]*2))
     return result 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', dest='model', action='store', required=True)    
+    parser.add_argument('--color', dest='color', action='store', required=True)
     args = parser.parse_args()
     model = load_model(args.model)
 
@@ -72,7 +46,7 @@ if __name__ == '__main__':
         # by frame
         ret, frame = vid.read()
         
-        frame = process_frame_color(frame, model)
+        frame = process_frame_color(frame, model,args.color)
         
         # Display the resulting frame
         cv2.imshow('frame', frame)
