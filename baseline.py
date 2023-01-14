922a9a138a63fc080f6ef8f69010bbee650674c0
Kainasu <Kainasu@github.com>
2023-01-14 15:25:21 +0100

    Fix baseline

--- baseline.py
+++ baseline.py
@@ -1,13 +1,11 @@
 import os
 from keras import backend as K
 from keras.models import Model
-from keras.layers import Input, Conv2D, Lambda
+from keras.layers import Input, Lambda
 from keras.losses import BinaryCrossentropy
 from keras.metrics import BinaryIoU
-import tensorflow as tf
-import cv2
-import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 def create_backgroundBaseline(image_size=(128,128,3)):    
     # Create the model
@@ -31,13 +29,23 @@ def create_hairBaseline(image_size=(128,128,3)):
 
 if __name__ == '__main__':
 
-    backgroundBaseline_path = 'models/baseline/onlyBlack'
+    parser = argparse.ArgumentParser()
+
+    # Add argument for input size
+    parser.add_argument('--size', type=int,
+                    help='Input size', default=128)
+
+    args = parser.parse_args()
+    
+    image_size = (args.size, args.size, 3)
+
+    backgroundBaseline_path = f'models/baseline/onlyBlack/{args.size}x{args.size}'
     os.makedirs(backgroundBaseline_path, exist_ok=True)
-    whitegroundBaseline_path = 'models/baseline/onlyWhite'
+    whitegroundBaseline_path = f'models/baseline/onlyWhite/{args.size}x{args.size}'
     os.makedirs(whitegroundBaseline_path, exist_ok=True)
 
-    baseline1 = create_backgroundBaseline((128,128,3))
+    baseline1 = create_backgroundBaseline(image_size=image_size)
     baseline1.save(os.path.join(backgroundBaseline_path, 'model.h5'))
 
-    baseline2 = create_hairBaseline((128,128,3))
+    baseline2 = create_hairBaseline(image_size=image_size)
     baseline2.save(os.path.join(whitegroundBaseline_path, 'model.h5'))
--- train_all_model.sh
+++ train_all_model.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-python3 run_train_segmentation.py --dataset Figaro1k --model-type unet --augmentation
-python3 run_train_segmentation.py --dataset Figaro1k --model-type unet --no-augmentation
-python3 run_train_segmentation.py --dataset Lfw --model-type unet --augmentation
-python3 run_train_segmentation.py --dataset Lfw --model-type unet --no-augmentation
-python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type unet --augmentation
-python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type unet --no-augmentation
+if [ "$#" -ne 1 ]; then
+    image_size=128
+else
+    image_size=$1
+fi
 
-python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --augmentation
-python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --no-augmentation
-python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --augmentation
-python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --no-augmentation
-python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --augmentation
-python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --no-augmentation
+python3 run_train_segmentation.py --dataset Figaro1k --model-type unet --augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Figaro1k --model-type unet --no-augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw --model-type unet --augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw --model-type unet --no-augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type unet --augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type unet --no-augmentation --size $image_size
 
-python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --augmentation --pretrained
-python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --no-augmentation --pretrained
-python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --augmentation --pretrained
-python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --no-augmentation --pretrained
-python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --augmentation --pretrained
-python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --no-augmentation --pretrained
+python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --no-augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --no-augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --augmentation --size $image_size
+python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --no-augmentation --size $image_size
 
-python3 baseline.py
+python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --augmentation --pretrained --size $image_size
+python3 run_train_segmentation.py --dataset Figaro1k --model-type mobile_unet --no-augmentation --pretrained --size $image_size
+python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --augmentation --pretrained --size $image_size
+python3 run_train_segmentation.py --dataset Lfw --model-type mobile_unet --no-augmentation --pretrained --size $image_size
+python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --augmentation --pretrained --size $image_size
+python3 run_train_segmentation.py --dataset Lfw+Figaro1k --model-type mobile_unet --no-augmentation --pretrained --size $image_size
+
+python3 baseline.py --size $image_size
